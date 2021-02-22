f30838318da9fd26fc31033a5ee29b5a946b9735
Barış Emren <45763450+barisemren@users.noreply.github.com>
2021-02-23 01:06:24 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+function scroll1(){
+    var elmnt = document.getElementById("content");
+    elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
+function scroll2(){
+    var elmnt = document.getElementById("aboutpage");
+    elmnt.scrollIntoView({behavior:"smooth",block:"center"});
+}
+function scroll3(){
+    var elmnt = document.getElementById("educationpage");
+    elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
+function scroll4(){
+    var elmnt = document.getElementById("cvpage");
+    elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
+function scroll5(){
+    var elmnt = document.getElementById("hobbiespage");
+    elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
+function scroll6(){
+    var elmnt = document.getElementById("contactpage");
+    elmnt.scrollIntoView({behavior:"smooth",block:"start"});
+}
