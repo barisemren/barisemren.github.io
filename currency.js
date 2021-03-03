c63294831d2edab7a13e8130cbb344c9c75ea2a5
Barış Emren <45763450+barisemren@users.noreply.github.com>
2021-03-04 02:02:31 +0300

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,17 +1,16 @@
 document.addEventListener("DOMContentLoaded",function(){
 
     document.querySelector("form").onsubmit=function(){
-        fetch("https://api.exchangeratesapi.io/latest?base=USD")
+        let link = "https://api.exchangeratesapi.io/latest?base=";
+        let base = document.querySelector("#basecurrency").value;
+        let target = document.querySelector("#targetcurrency").value;
+        let url = link + base;
+
+        fetch(url)
         .then(response => response.json())
         .then(data => {
-            const currency = document.getElementById("currency").value.toUpperCase();
-            const rate = data.rates[currency];
-            if(rate!==undefined){
-                document.getElementById("result").innerHTML=`1 USD = ${rate} ${currency}`;
-            }
-            else{
-                document.getElementById("result").innerHTML=`Invalid Currency`;
-            }
+            const rate = data.rates[target];
+            document.getElementById("result").innerHTML=`1 ${base} = ${rate} ${target}`;
         });
         return false;
 
